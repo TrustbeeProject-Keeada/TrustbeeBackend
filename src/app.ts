5276e07be512679168c19b9a539ce6d439cebd00
HamodyArc <mohamad.alaktaa@elev.ga.ntig.se>
2026-03-18 14:40:07 +0100

    first working user commit

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,13 @@
 import express, { type Request, type Response } from "express";
-import console = require("node:console");
 import userRoutes from "./routes/user.routes";
-import { errorHandler } from "./middlewere/error.middlewere";
-import newsRoutes from "./routes/product.routes";
-import authRoutes from "./routes/auth.routes";
-import orderRoutes from "./routes/orders.routes";
+import { errorHandler } from "./middleware/error.middleware";
 
 export const createApp = () => {
   const app = express();
 
   app.use(express.json());
 
-  app.use("/api", authRoutes);
   app.use("/api", userRoutes);
-  app.use("/api", newsRoutes);
-  app.use("/api", orderRoutes);
 
   app.use(errorHandler);
 
