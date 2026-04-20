a13a52b62fe217d59b1bbfff3b8ab62ca7260a5f
TapasPulis <rooneysamragya10@gmail.com>
2026-04-20 13:24:23 +0200

    Updated swedish text to English

--- src/models/support.model.ts
+++ src/models/support.model.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 export const supportTicketSchema = z.object({
   body: z
     .object({
-      firstname: z.string().min(2, "Förnamnet är för kort"),
-      lastname: z.string().min(2, "Efternamnet är för kort"),
-      email: z.string().email("Ange en giltig e-postadress"),
+      firstname: z.string().min(2, "The firstname is too short"),
+      lastname: z.string().min(2, "The lastname is too short"),
+      email: z.string().email("Please enter a valid email address"),
       message: z
         .string()
-        .min(10, "Meddelandet måste vara minst 10 tecken långt"),
+        .min(10, "The message must be at least 10 characters long"),
       sendAsEmail: z.boolean().optional().default(false),
     })
     .strict(),
